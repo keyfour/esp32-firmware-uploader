34a6caad21709d79021ba850aa73152f0fcfda34
Alex Karpov <keyfour13@gmail.com>
2020-07-16 07:56:27 +0300

    Fix tests

--- backend/tests/storage_test.py
+++ backend/tests/storage_test.py
@@ -16,28 +16,28 @@ class TestStorageMethods(unittest.TestCase):
     self.path_prefix = TMP_DIR
     self.local_json_storage = LocalJsonStorage(self.path_prefix)
     self.valid_json = self.VALID_JSON
-    self.invalid_json = self.valid_json.replace('"','\"')
-    self.valid_json_path = self.path_prefix.join('/valid.json')
-    self.invalid_json_path = self.path_prefix.join('/invalid.json')
+    self.invalid_json = self.valid_json.replace('"','\\"')
+    self.valid_json_path = self.path_prefix + '/valid.json'
+    self.invalid_json_path = self.path_prefix + '/invalid.json'
 
   def test_local_json_storage_paths(self):
-    self.assertEqual(self.path_prefix.join('/boards.json'),
+    self.assertEqual(self.path_prefix + '/boards.json',
       self.local_json_storage.boards_file)
-    self.assertEqual(self.path_prefix.join('/frameworks.json'),
+    self.assertEqual(self.path_prefix + '/frameworks.json',
       self.local_json_storage.frameworks_file)
 
   def test_local_json_storage_parse_file(self):
-    with open(self.valid_json_path) as f:
+    with open(self.valid_json_path, "w") as f:
       f.write(self.valid_json)
-    with open(self.invalid_json_path) as f:
+    with open(self.invalid_json_path, "w") as f:
       f.write(self.invalid_json)
     result = self.local_json_storage.parse_file(self.valid_json_path)
-    self.assertEqual(isinstance(result), list)
-    self.assertEqual(result[0]['board'], "ESP32")
-    self.assertEqual(result[0]['name'], "Device")
-    self.assertEqual(result[0]['start'], 0)
+    self.assertTrue(isinstance(result, dict))
+    self.assertEqual(result['board'], "ESP32")
+    self.assertEqual(result['name'], "Device")
+    self.assertEqual(result['start'], 0)
     result = self.local_json_storage.parse_file(self.invalid_json_path)
-    self.assertEqual(isinstance(result), list)
+    self.assertTrue(isinstance(result, dict))
     self.assertEqual(len(result), 0)
 
   def tearDown(self):
