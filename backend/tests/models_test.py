8ba2551009d060cb839d367424772841070ecbfe
Alex Karpov <keyfour13@gmail.com>
2020-07-22 09:17:15 +0300

    Add tests for model

--- backend/tests/models_test.py
+++ backend/tests/models_test.py
@@ -1,10 +1,56 @@
 import unittest
 from uploader.models import ESP32FirmwareUploader
 
+PORTS = [
+        {
+          'id': 0,
+          'name': '/dev/ttyUSB0',
+        },
+        {
+          'id': 1,
+          'name': '/dev/ttyACM0',
+        },
+        {
+          'id': 2,
+          'name': '/dev/ttyS0',
+        }]
+
+BOARDS = [
+        {
+          'id': 0,
+          'name': 'ESP32-WROOM',
+        },
+        {
+          'id': 1,
+          'name': 'ESP32-CAM',
+        },
+        {
+          'id': 2,
+          'name': 'Heltec ESP32s',
+        },
+      ]
+
+FRAMEWORKS = [
+        {
+          'id': 0,
+          'name': 'Arduino',
+        },
+        {
+          'id': 1,
+          'name': 'ESP-IDF',
+        },
+      ]
+
 class ConnectorMock:
 
+  def __init__(self):
+    self.is_empty = False
+
   def ports(self):
-    pass
+    if not self.is_empty:
+      return PORTS
+    else:
+      return []
 
 class StorageMock:
 
@@ -21,6 +67,20 @@ class TestModelsMethods(unittest.TestCase):
     self.storage = StorageMock()
     self.model = ESP32FirmwareUploader(self.storage, self.connector)
 
+  def test_ports_return_correct(self):
+    self.connector.is_empty = False
+    ports = self.model.ports()
+    self.assertTrue(isinstance(ports, list))
+    self.assertEqual(len(ports), len(PORTS))
+    for i, port in enumerate(ports):
+      self.assertEqual(port['id'], PORTS[i]['id'])
+      self.assertEqual(port['name'], PORTS[i]['name'])
+
+  def test_ports_return_empty(self):
+    self.connector.is_empty = True
+    ports = self.model.ports()
+    self.assertTrue(isinstance(ports, list))
+    self.assertEqual(len(ports), 0)
 
 if __name__ == "__main__":
     unittest.main()
