dd0ff843cc7c5c871680a98a8a8ad36fd99b216f
Alex Karpov <keyfour13@gmail.com>
2020-07-23 09:20:32 +0300

    Add tests for model

--- backend/tests/models_test.py
+++ backend/tests/models_test.py
@@ -54,11 +54,20 @@ class ConnectorMock:
 
 class StorageMock:
 
+  def __init__(self):
+    self.is_empty = False
+
   def boards(self):
-    pass
+    if not self.is_empty:
+      return BOARDS
+    else:
+      return []
 
   def frameworks(self):
-    pass
+    if not self.is_empty:
+      return FRAMEWORKS
+    else:
+      return []
 
 class TestModelsMethods(unittest.TestCase):
 
@@ -66,21 +75,46 @@ class TestModelsMethods(unittest.TestCase):
     self.connector = ConnectorMock()
     self.storage = StorageMock()
     self.model = ESP32FirmwareUploader(self.storage, self.connector)
+    self.list_keys = ["id", "name"]
 
   def test_ports_return_correct(self):
     self.connector.is_empty = False
     ports = self.model.ports()
-    self.assertTrue(isinstance(ports, list))
-    self.assertEqual(len(ports), len(PORTS))
-    for i, port in enumerate(ports):
-      self.assertEqual(port['id'], PORTS[i]['id'])
-      self.assertEqual(port['name'], PORTS[i]['name'])
+    self.compare_lists(ports, PORTS, self.list_keys)
 
   def test_ports_return_empty(self):
     self.connector.is_empty = True
     ports = self.model.ports()
-    self.assertTrue(isinstance(ports, list))
-    self.assertEqual(len(ports), 0)
+    self.compare_lists(ports, [])
+
+  def tets_boards_return(self):
+    self.storage.is_empty = False
+    boards = self.model.boards()
+    self.compare_lists(boards, BOARDS, self.list_keys)
+
+  def test_boards_return_empty(self):
+    self.storage.is_empty = True
+    boards = self.model.boards()
+    self.compare_lists(boards, [])
+
+  def test_frameworks_return(self):
+    self.storage.is_empty = False
+    frameworks = self.model.frameworks()
+    self.compare_lists(frameworks, FRAMEWORKS, self.list_keys)
+
+  def test_frameworks_return_empty(self):
+    self.storage.is_empty = True
+    frameworks = self.model.frameworks()
+    self.compare_lists(frameworks, [])
+
+
+  def compare_lists(self, list0, list1, keys=[]):
+    self.assertTrue(isinstance(list0, list))
+    self.assertTrue(isinstance(list1, list))
+    self.assertEqual(len(list0), len(list1))
+    for item0, item1 in zip(list0, list1):
+      for key in keys:
+        self.assertEqual(item0[key], item1[key])
 
 if __name__ == "__main__":
     unittest.main()
